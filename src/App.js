cb8578799b8be1f6339992cd63510f32dd7a777e
Bahran <Bahran840@hotmail.com>
2018-07-30 12:48:14 -0700

    changed background color & fixed signin functionality

--- src/App.js
+++ src/App.js
@@ -35,6 +35,13 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
 
     }
   }
@@ -43,6 +50,17 @@ class App extends Component {
     fetch('http://localhost:3000')
       .then(response => response.json())
       .then(console.log)
+  }
+
+  loadUser = (data) => {
+    this.setState({ user: {
+                id: data.id,
+                name: data.name,
+                email: data.email,
+                entries: data.entries,
+                joined: data.joined
+              }
+    })
   }
 
   calculateFaceLocation = (data) => {
@@ -99,7 +117,7 @@ class App extends Component {
         { route === 'home'
             ? <div>
                 <Logo />
-                <Rank />
+                <Rank name={this.state.user.name} entries={this.state.user.entries}/>
                 <ImageLinkForm
                   onInputChange={this.onInputChange}
                   onButtonSubmit={this.onButtonSubmit}
@@ -108,8 +126,8 @@ class App extends Component {
               </div>
             : (
                route === 'signin' || route === 'signout'
-               ? <Signin onRouteChange={this.onRouteChange} />
-               : <Register onRouteChange={this.onRouteChange} />
+               ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+               : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
              )
         }
       </div>
